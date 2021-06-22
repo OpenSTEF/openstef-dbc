16e3b3a3dae51a7a56c7850fbec6f5019a62052c
Frank Kreuwel <frank.kreuwel@alliander.com>
2021-06-22 08:21:14 +0200

    add __version__

--- openstf_dbc/__init__.py
+++ openstf_dbc/__init__.py
@@ -1,3 +1,11 @@
 # SPDX-FileCopyrightText: 2021 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com>
 #
 # SPDX-License-Identifier: MPL-2.0
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    # package is not installed
+    pass
