609fb28c26cd1a09f749539fbf3bfe22d700a562
Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>
2021-07-14 15:57:07 +0200

    Update test_ems.py

--- tests/unit/services/test_ems.py
+++ tests/unit/services/test_ems.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
 import unittest
 from datetime import datetime, timedelta
 
