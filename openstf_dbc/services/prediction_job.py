aa525bbe63864419e71a4287222f122c335e61a4
Bram Harmsen <bram.harmsen@alliander.com>
2021-08-30 10:20:08 +0200

    reorder method; pulbic, private, static

--- openstf_dbc/services/prediction_job.py
+++ openstf_dbc/services/prediction_job.py
@@ -16,79 +16,6 @@ class PredictionJob:
     def __init__(self):
         self.logger = logging.get_logger(self.__class__.__name__)
 
-    def _add_description_to_prediction_job(self, prediction_job):
-        return self._add_description_to_prediction_jobs([prediction_job])[0]
-
-    def _add_description_to_prediction_jobs(self, prediction_jobs):
-
-        for prediction_job in prediction_jobs:
-            systems = Systems().get_systems_by_pid(
-                pid=prediction_job["id"], return_list=True
-            )
-            systems_str = "+".join([s["system_id"] for s in systems])
-            prediction_job["description"] = systems_str
-
-        return prediction_jobs
-
-    def _add_model_type_group_to_prediction_jobs(self, prediction_jobs):
-        for prediction_job in prediction_jobs:
-            # TODO this needs to be changed in the
-            if "quantile" in prediction_job["model"]:
-                prediction_job["model_type_group"] = "quantile"
-            else:
-                prediction_job["model_type_group"] = "default"
-
-        return prediction_jobs
-
-    def _add_model_type_group_to_prediction_job(self, prediction_job):
-        return self._add_model_type_group_to_prediction_jobs([prediction_job])[0]
-
-    def _add_quantiles_to_prediciton_job(self, prediction_job):
-        return self._add_quantiles_to_prediciton_jobs([prediction_job])[0]
-
-    def _add_quantiles_to_prediciton_jobs(self, prediction_jobs):
-        prediction_job_ids = [pj["id"] for pj in prediction_jobs]
-        prediction_jobs_ids_str = ", ".join([f"'{p}'" for p in prediction_job_ids])
-
-        query = f"""
-            SELECT p.id AS prediction_id, qs.quantiles
-            FROM quantile_sets AS qs
-            JOIN (
-                predictions_quantile_sets AS pq,
-                predictions as p
-            )
-            WHERE
-                p.id = pq.prediction_id AND
-                qs.id = pq.quantile_set_id AND
-                p.id IN  ({prediction_jobs_ids_str})
-            ORDER BY prediction_id
-            ;
-        """
-        result = _DataInterface.get_instance().exec_sql_query(query)
-
-        prediction_job_quantiles = {}
-
-        # get quantiles for every prediction job
-        for _, row in result.iterrows():
-            pid = row["prediction_id"]
-
-            if pid not in prediction_job_quantiles:
-                prediction_job_quantiles[pid] = []
-
-            prediction_job_quantiles[pid] += json.loads(row["quantiles"])
-
-        # add quantiles to prediction job
-        for prediction_job in prediction_jobs:
-            pid = prediction_job["id"]
-            # add quantiles if any
-            if pid in prediction_job_quantiles:
-                prediction_job["quantiles"] = sorted(prediction_job_quantiles[pid])
-                continue
-            # add empty list if none (this should not actually happen)
-            prediction_job["quantiles"] = []
-
-        return prediction_jobs
-
     def get_prediction_job(self, pid, model_type=None, is_active=None):
         """Get prediction job for a given pid from the database.
 
@@ -169,6 +96,220 @@ class PredictionJob:
 
         # Add model group
         prediction_jobs = self._add_model_type_group_to_prediction_jobs(prediction_jobs)
+
+        return prediction_jobs
+
+    def get_prediction_jobs_wind(self):
+        query = """
+            SELECT
+                p.id, p.forecast_type, p.model, p.horizon_minutes, p.resolution_minutes,
+                p.name,
+                min(s.sid) as sid,
+                w.lat as lat,
+                w.lon as lon,
+                w.turbine_type,
+                w.n_turbines,
+                w.hub_height
+            FROM predictions as p
+            LEFT JOIN predictions_systems as ps ON p.id = ps.prediction_id
+            LEFT JOIN systems as s ON s.sid = ps.system_id
+            LEFT JOIN windspecs as w ON p.id = w.pid
+            WHERE p.forecast_type = 'wind' AND p.active = 1
+            GROUP BY p.id
+        """
+
+        result = _DataInterface.get_instance().exec_sql_query(query)
+
+        # Convert to list of dictionaries
+        result = result.to_dict(orient="records")
+
+        # Add description field
+        for pj in result:
+            systems = Systems().get_systems_by_pid(pj["id"])
+            pj["description"] = "+".join(list(systems.system_id))
+        return result
+
+    def get_prediction_jobs_solar(self):
+        query = """
+            SELECT
+                p.id, p.forecast_type, p.model, p.horizon_minutes, p.resolution_minutes,
+                p.name,
+                min(s.lat) as lat,
+                min(s.lon) as lon,
+                min(s.sid) as sid,
+                ss.lat,
+                ss.lon,
+                ss.radius,
+                ss.peak_power
+            FROM predictions as p
+            LEFT JOIN predictions_systems as ps ON p.id = ps.prediction_id
+            LEFT JOIN systems as s ON s.sid = ps.system_id
+            LEFT JOIN solarspecs as ss ON p.id = ss.pid
+            WHERE p.forecast_type = 'solar' AND p.active = 1
+            GROUP BY p.id
+        """
+
+        result = _DataInterface.get_instance().exec_sql_query(query)
+
+        # Convert to list of dictionaries
+        result = result.to_dict(orient="records")
+
+        # Add description field
+        for pj in result:
+            systems = Systems().get_systems_by_pid(pj["id"])
+            pj["description"] = "+".join(list(systems.system_id))
+        return result
+
+    def get_hyper_params(self, pj):
+        """Method that finds the latest hyperparameters for a specific prediction job.
+        Args:
+            pj: Prediction job (dict).
+
+        Returns:
+            (dict) params: Dictionary with hyperparameters.
+                Empty if no hyperparameters excist or in case of errors.
+        """
+        # Compose query
+        query = f"""
+            SELECT hp.name, hpv.value
+            FROM hyper_params hp
+            LEFT JOIN hyper_param_values hpv
+                ON hpv.hyper_params_id=hp.id
+            WHERE hpv.prediction_id="{pj["id"]}" AND hp.model="{pj["model"]}"
+        """
+        # Default params is empty dict
+        params = {}
+
+        try:
+            # Execute query
+            result = _DataInterface.get_instance().exec_sql_query(query)
+            # Convert result to dict with proper keys
+            params = result.set_index("name").to_dict()["value"]
+        except Exception as e:
+            self.logger.error(
+                "Error occured while retrieving hyper parameters",
+                exc_info=e,
+                pid=pj["id"],
+            )
+
+        return params
+
+    def get_hyper_params_last_optimized(self, pj):
+        """Method that finds the date of the most recent hyperparameters
+        Args:
+            pj: Prediction job (dict).
+        Returns:
+            (datetime) last: Datetime of last hyperparameters
+        """
+        query = f"""
+            SELECT MAX(hpv.created) as last
+            FROM hyper_params hp
+            LEFT JOIN hyper_param_values hpv
+                ON hpv.hyper_params_id=hp.id
+            WHERE hpv.prediction_id={pj["id"]} AND hp.model="{pj["model"]}"
+        """
+        last = None
+        try:
+            # Execute query
+            result = _DataInterface.get_instance().exec_sql_query(query)
+            # Convert result datetime instance
+            last = result["last"][0].to_pydatetime()
+            # If dictionary is empty raise exception and fall back to defaults
+        except Exception as e:
+            self.logger.error(
+                "Could not retrieve last hyperparemeters from database "
+                f'for pid {pj["id"]}',
+                exc_info=e,
+            )
+
+        return last
+
+    def get_featureset(self, featureset_name):
+
+        if featureset_name not in FEATURESET_NAMES:
+            raise KeyError(
+                f"Unknown featureset name '{featureset_name}'. "
+                f"Valid names are {', '.join(FEATURESET_NAMES)}"
+            )
+
+        return FEATURESETS[featureset_name]
+
+    def get_featuresets(self):
+        return FEATURESETS
+
+    def get_featureset_names(self):
+        return FEATURESET_NAMES
+
+    def _add_description_to_prediction_job(self, prediction_job):
+        return self._add_description_to_prediction_jobs([prediction_job])[0]
+
+    def _add_description_to_prediction_jobs(self, prediction_jobs):
+
+        for prediction_job in prediction_jobs:
+            systems = Systems().get_systems_by_pid(
+                pid=prediction_job["id"], return_list=True
+            )
+            systems_str = "+".join([s["system_id"] for s in systems])
+            prediction_job["description"] = systems_str
+
+        return prediction_jobs
+
+    def _add_model_type_group_to_prediction_jobs(self, prediction_jobs):
+        for prediction_job in prediction_jobs:
+            # TODO this needs to be changed in the
+            if "quantile" in prediction_job["model"]:
+                prediction_job["model_type_group"] = "quantile"
+            else:
+                prediction_job["model_type_group"] = "default"
+
+        return prediction_jobs
+
+    def _add_model_type_group_to_prediction_job(self, prediction_job):
+        return self._add_model_type_group_to_prediction_jobs([prediction_job])[0]
+
+    def _add_quantiles_to_prediciton_job(self, prediction_job):
+        return self._add_quantiles_to_prediciton_jobs([prediction_job])[0]
+
+    def _add_quantiles_to_prediciton_jobs(self, prediction_jobs):
+        prediction_job_ids = [pj["id"] for pj in prediction_jobs]
+        prediction_jobs_ids_str = ", ".join([f"'{p}'" for p in prediction_job_ids])
+
+        query = f"""
+            SELECT p.id AS prediction_id, qs.quantiles
+            FROM quantile_sets AS qs
+            JOIN (
+                predictions_quantile_sets AS pq,
+                predictions as p
+            )
+            WHERE
+                p.id = pq.prediction_id AND
+                qs.id = pq.quantile_set_id AND
+                p.id IN  ({prediction_jobs_ids_str})
+            ORDER BY prediction_id
+            ;
+        """
+        result = _DataInterface.get_instance().exec_sql_query(query)
+
+        prediction_job_quantiles = {}
+
+        # get quantiles for every prediction job
+        for _, row in result.iterrows():
+            pid = row["prediction_id"]
+
+            if pid not in prediction_job_quantiles:
+                prediction_job_quantiles[pid] = []
+
+            prediction_job_quantiles[pid] += json.loads(row["quantiles"])
+
+        # add quantiles to prediction job
+        for prediction_job in prediction_jobs:
+            pid = prediction_job["id"]
+            # add quantiles if any
+            if pid in prediction_job_quantiles:
+                prediction_job["quantiles"] = sorted(prediction_job_quantiles[pid])
+                continue
+            # add empty list if none (this should not actually happen)
+            prediction_job["quantiles"] = []
 
         return prediction_jobs
 
@@ -268,144 +409,3 @@ class PredictionJob:
         else:
             raise ValueError("external_id should be str or list of str")
         return f"p.external_id IN ({in_values})"
-
-    def get_prediction_jobs_wind(self):
-        query = """
-            SELECT
-                p.id, p.forecast_type, p.model, p.horizon_minutes, p.resolution_minutes,
-                p.name,
-                min(s.sid) as sid,
-                w.lat as lat,
-                w.lon as lon,
-                w.turbine_type,
-                w.n_turbines,
-                w.hub_height
-            FROM predictions as p
-            LEFT JOIN predictions_systems as ps ON p.id = ps.prediction_id
-            LEFT JOIN systems as s ON s.sid = ps.system_id
-            LEFT JOIN windspecs as w ON p.id = w.pid
-            WHERE p.forecast_type = 'wind' AND p.active = 1
-            GROUP BY p.id
-        """
-
-        result = _DataInterface.get_instance().exec_sql_query(query)
-
-        # Convert to list of dictionaries
-        result = result.to_dict(orient="records")
-
-        # Add description field
-        for pj in result:
-            systems = Systems().get_systems_by_pid(pj["id"])
-            pj["description"] = "+".join(list(systems.system_id))
-        return result
-
-    def get_prediction_jobs_solar(self):
-        query = """
-            SELECT
-                p.id, p.forecast_type, p.model, p.horizon_minutes, p.resolution_minutes,
-                p.name,
-                min(s.lat) as lat,
-                min(s.lon) as lon,
-                min(s.sid) as sid,
-                ss.lat,
-                ss.lon,
-                ss.radius,
-                ss.peak_power
-            FROM predictions as p
-            LEFT JOIN predictions_systems as ps ON p.id = ps.prediction_id
-            LEFT JOIN systems as s ON s.sid = ps.system_id
-            LEFT JOIN solarspecs as ss ON p.id = ss.pid
-            WHERE p.forecast_type = 'solar' AND p.active = 1
-            GROUP BY p.id
-        """
-
-        result = _DataInterface.get_instance().exec_sql_query(query)
-
-        # Convert to list of dictionaries
-        result = result.to_dict(orient="records")
-
-        # Add description field
-        for pj in result:
-            systems = Systems().get_systems_by_pid(pj["id"])
-            pj["description"] = "+".join(list(systems.system_id))
-        return result
-
-    def get_hyper_params_last_optimized(self, pj):
-        """Method that finds the date of the most recent hyperparameters
-        Args:
-            pj: Prediction job (dict).
-        Returns:
-            (datetime) last: Datetime of last hyperparameters
-        """
-        query = f"""
-            SELECT MAX(hpv.created) as last
-            FROM hyper_params hp
-            LEFT JOIN hyper_param_values hpv
-                ON hpv.hyper_params_id=hp.id
-            WHERE hpv.prediction_id={pj["id"]} AND hp.model="{pj["model"]}"
-        """
-        last = None
-        try:
-            # Execute query
-            result = _DataInterface.get_instance().exec_sql_query(query)
-            # Convert result datetime instance
-            last = result["last"][0].to_pydatetime()
-            # If dictionary is empty raise exception and fall back to defaults
-        except Exception as e:
-            self.logger.error(
-                "Could not retrieve last hyperparemeters from database "
-                f'for pid {pj["id"]}',
-                exc_info=e,
-            )
-
-        return last
-
-    def get_hyper_params(self, pj):
-        """Method that finds the latest hyperparameters for a specific prediction job.
-        Args:
-            pj: Prediction job (dict).
-
-        Returns:
-            (dict) params: Dictionary with hyperparameters.
-                Empty if no hyperparameters excist or in case of errors.
-        """
-        # Compose query
-        query = f"""
-            SELECT hp.name, hpv.value
-            FROM hyper_params hp
-            LEFT JOIN hyper_param_values hpv
-                ON hpv.hyper_params_id=hp.id
-            WHERE hpv.prediction_id="{pj["id"]}" AND hp.model="{pj["model"]}"
-        """
-        # Default params is empty dict
-        params = {}
-
-        try:
-            # Execute query
-            result = _DataInterface.get_instance().exec_sql_query(query)
-            # Convert result to dict with proper keys
-            params = result.set_index("name").to_dict()["value"]
-        except Exception as e:
-            self.logger.error(
-                "Error occured while retrieving hyper parameters",
-                exc_info=e,
-                pid=pj["id"],
-            )
-
-        return params
-
-    def get_featureset(self, featureset_name):
-
-        if featureset_name not in FEATURESET_NAMES:
-            raise KeyError(
-                f"Unknown featureset name '{featureset_name}'. "
-                f"Valid names are {', '.join(FEATURESET_NAMES)}"
-            )
-
-        return FEATURESETS[featureset_name]
-
-    def get_featuresets(self):
-        return FEATURESETS
-
-    def get_featureset_names(self):
-        return FEATURESET_NAMES
