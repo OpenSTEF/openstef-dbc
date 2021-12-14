0afc49be3ba4e119e465c750f9f4fb17ab817359
Frank Kreuwel <frank.kreuwel@alliander.com>
2021-12-14 13:16:20 +0100

    bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_long_description_from_readme():
 
 setup(
     name="openstef_dbc",
-    version="3.0.0",
+    version="3.0.1",
     packages=find_packages(include=["openstef_dbc", "openstef_dbc.*"]),
     description="Database Connection for OpenSTEF",
     long_description=read_long_description_from_readme(),
