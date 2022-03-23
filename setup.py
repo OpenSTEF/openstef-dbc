50b1208329c4281946ef467d0b328858e7e01bca
David Swinkels <david.swinkels@alliander.com>
2022-03-23 13:24:55 +0100

    Update SQLalchemy and openstef

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_long_description_from_readme():
 
 setup(
     name="openstef_dbc",
-    version="3.1.6",
+    version="3.1.6a",
     packages=find_packages(include=["openstef_dbc", "openstef_dbc.*"]),
     description="Database Connection for OpenSTEF",
     long_description=read_long_description_from_readme(),
