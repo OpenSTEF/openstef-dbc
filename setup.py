62f7cbb819c27fd70825b68800f8bb75a2c9a547
JanMaartenvanDoorn <jan.maarten.van.doorn@alliander.com>
2023-03-27 13:41:33 +0200

    bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_long_description_from_readme():
 
 setup(
     name="openstef_dbc",
-    version="3.5.3",
+    version="3.5.4",
     packages=find_packages(include=["openstef_dbc", "openstef_dbc.*"]),
     description="Database Connection for OpenSTEF",
     long_description=read_long_description_from_readme(),
