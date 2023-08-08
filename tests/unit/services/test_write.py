0bd60c079e861241bc1045070348aa52f56c2d27
black <action@github.com>
2023-08-08 14:40:54 +0000

    Format Python code with Black
    Signed-off-by: black <action@github.com>

--- tests/unit/services/test_write.py
+++ tests/unit/services/test_write.py
@@ -14,7 +14,6 @@ datetime_start = datetime.fromisoformat("2019-01-01 10:00:00")
 datetime_end = datetime.fromisoformat("2019-01-01 10:15:00")
 
 
-
 class TestWriteService(unittest.TestCase):
     def setUp(self) -> None:
         return super().setUp()
@@ -22,23 +21,27 @@ class TestWriteService(unittest.TestCase):
     @patch("openstef_dbc.services.write._DataInterface")
     def test_write_forecast_tahead_happy_flow(self, data_interface_mock):
         """Test happy flow of writing forecasts. The actual writing is mocked"""
-        data_interface_mock.get_instance.return_value.exec_influx_write.return_value = True
-        
+        data_interface_mock.get_instance.return_value.exec_influx_write.return_value = (
+            True
+        )
+
         # Arange
         writer = Write()
         example_data = pd.DataFrame(
-            index=pd.to_datetime(['2023-01-01 10:00:00+00:00','2023-01-01 10:15:00+00:00']),
-            data=dict(forecast=[1.1,2.0],
-                      pid=[1,1],
-                      stdev=[0,0],
-                      description=['test','test'],
-                      customer=['example','example'],
-                      quality=['actual','actual'],
-            )
+            index=pd.to_datetime(
+                ["2023-01-01 10:00:00+00:00", "2023-01-01 10:15:00+00:00"]
+            ),
+            data=dict(
+                forecast=[1.1, 2.0],
+                pid=[1, 1],
+                stdev=[0, 0],
+                description=["test", "test"],
+                customer=["example", "example"],
+                quality=["actual", "actual"],
+            ),
         )
         data_interface_mock()
-        
-        writer.write_forecast(data=example_data,
-                             t_ahead_series=True)
-        
+
+        writer.write_forecast(data=example_data, t_ahead_series=True)
+
         assert writer
