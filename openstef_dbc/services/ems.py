d0a279105d704b5cb0881a4560b93d932cf760d7
Martijn Cazemier <37078892+MartijnCa@users.noreply.github.com>
2023-11-20 19:39:14 +0100

    Make get_load_sid more robust: prevent accidently naming 'nEntries' column 'load' and vice versa
    Signed-off-by: Martijn Cazemier <37078892+MartijnCa@users.noreply.github.com>

--- openstef_dbc/services/ems.py
+++ openstef_dbc/services/ems.py
@@ -130,9 +130,9 @@ class Ems:
             result = pd.concat(
                 [
                     result_raw[0][["_time", "_value"]].rename(
-                        columns={"_value": "load"}
+                        columns={"_value": result_raw[0]["result"][0]}
                     ),
-                    result_raw[1][["_value"]].rename(columns={"_value": "nEntries"}),
+                    result_raw[1][["_value"]].rename(columns={"_value": result_raw[1]["result"][0]}),
                 ],
                 axis=1,
             ).set_index("_time")
