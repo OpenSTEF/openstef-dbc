68287eb910e4f50220f75504fd5e0cfe819d81a0
Clara De Smet <157587243+clara-de-smet@users.noreply.github.com>
2025-01-27 11:48:11 +0100

    chore: Bumped version
    Signed-off-by: Clara De Smet <157587243+clara-de-smet@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_long_description_from_readme():
 
 setup(
     name="openstef_dbc",
-    version="3.7.18",
+    version="3.7.19",
     packages=find_packages(include=["openstef_dbc", "openstef_dbc.*"]),
     description="Database Connection for OpenSTEF",
     long_description=read_long_description_from_readme(),
