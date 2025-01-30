da9d1e9d7dfaf9439e9b995175bcec4163d10ec7
fortinmax <maxime.fortin@rte-france.com>
2025-01-30 16:20:41 +0100

    adding datetime converion before checking time zone
    Signed-off-by: fortinmax <maxime.fortin@rte-france.com>

--- openstef_dbc/services/weather.py
+++ openstef_dbc/services/weather.py
@@ -267,6 +267,9 @@ class Weather:
             print(df.head())
         """
 
+        datetime_start = pd.to_datetime(datetime_start)
+        datetime_end = pd.to_datetime(datetime_end)
+
         # Convert to UTC and remove UTC as note
         if datetime_start.tz is not None:
             datetime_start = datetime_start.tz_convert("UTC").tz_localize(None)
