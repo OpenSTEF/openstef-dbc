5ab6ec773bb03159d863f69ed0f57c7dc2ab334e
fortinmax <maxime.fortin@rte-france.com>
2025-03-07 12:30:28 +0100

    completing tests
    Signed-off-by: fortinmax <maxime.fortin@rte-france.com>

--- tests/integration/test_database_connection.py
+++ tests/integration/test_database_connection.py
@@ -15,6 +15,11 @@ from openstef_dbc.data_interface import _DataInterface
 from openstef_dbc.database import DataBase
 from openstef.data_classes.prediction_job import PredictionJobDataClass
 from openstef_dbc.services.prediction_job import PredictionJobRetriever
+from openstef_dbc.services.systems import Systems
+from openstef_dbc.services.model_input import ModelInput
+from openstef_dbc.services.splitting import Splitting
+from openstef_dbc.services.weather import Weather
+from openstef_dbc.services.write import Write
 from tests.integration.mock_influx_db_admin import MockInfluxDBAdmin
 
 from tests.integration.settings import Settings
@@ -54,25 +59,72 @@ class TestDataBaseConnexion(unittest.TestCase):
         assert isinstance(response, list)
 
     
-    # def test_get_ean_for_pid(self):
+    def test_get_ean_for_pid(self):
     
-    # def test_add_quantiles_to_prediction_jobs(self):
+        pj_retriever = PredictionJobRetriever()
+
+        response = pj_retriever.get_ean_for_pid(1)
+        assert isinstance(response, list)
+
     
-    # def test_get_systems_near_location(self):
-    
-    # def test_get_systems_by_pid(self):
-    
-    # def test_get_random_pv_systems(self):
-    
-    # def test_get_api_key_for_system(self):
-    
-    # def test_write_location(self):
-    
-    # def test_get_power_curve(self):
-    
-    # def test_get_energy_split_coefs(self):
-    
-    # def test_get_weather_forecast_locations(self):
-    
-    # def test_get_coordinates_of_location(self):
+    def test_add_quantiles_to_prediction_jobs(self):
         
+        pj_retriever = PredictionJobRetriever()
+        pjs = pj_retriever.get_prediction_jobs()       
+
+        response = pj_retriever._add_quantiles_to_prediction_jobs(pjs)[0]
+        assert isinstance(response, PredictionJobDataClass)
+        assert hasattr(response, 'quantiles')
+    
+    def test_get_systems_near_location(self):
+        
+        system = Systems()
+        response = system.get_systems_near_location(location=[0.0,0.0])       
+        assert isinstance(response, pd.DataFrame)
+    
+    def test_get_systems_by_pid(self):
+        system = Systems()
+        response = system.get_systems_by_pid(pid = 1)
+        assert isinstance(response, pd.DataFrame)
+    
+    def test_get_random_pv_systems(self):
+        system = Systems()
+        response = system.get_random_pv_systems()
+        assert isinstance(response, pd.DataFrame)
+        
+    def test_get_api_key_for_system(self):
+        system = Systems()
+        response = system.get_api_key_for_system(sid='1')     
+        assert isinstance(response, str)
+        
+    def test_write_location(self):
+        write = Write()
+        # Mocking sql write execution
+        # TODO : How to mock writing but keep testing SQLAchemy execute method ?
+        # response = write.write_location(location_name="Random City", location = (0.0,0.0))
+    
+    def test_get_power_curve(self):
+        modelinput = ModelInput()
+        response = modelinput.get_power_curve(turbine_type='Enercon E101')
+        assert isinstance(response, dict)
+        for key in ['name','cut_in','cut_off','kind','manufacturer','peak_capacity','rated_power','slope_center','steepness']:
+            assert key in response
+    
+    def test_get_energy_split_coefs(self):
+        splitting = Splitting()
+        pj_retriever = PredictionJobRetriever()
+        pj = pj_retriever.get_prediction_jobs()[0]
+        response = splitting.get_energy_split_coefs(pj=pj)
+        
+        assert isinstance(response, dict)
+        
+    def test_get_weather_forecast_locations(self):
+        weather = Weather()
+        response = weather.get_weather_forecast_locations()
+        
+        assert isinstance(response, list)
+        
+    def test_get_coordinates_of_location(self):
+        weather = Weather()
+        response = weather._get_coordinates_of_location(location_name = 'Leeuwarden')
+        assert isinstance(response, tuple)
