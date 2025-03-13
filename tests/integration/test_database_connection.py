f644044652d20f70d29442659cafd3c71aef7e17
maxi-fort <78433759+maxi-fort@users.noreply.github.com>
2025-03-13 22:08:30 +0100

    Update tests/integration/test_database_connection.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Signed-off-by: maxi-fort <78433759+maxi-fort@users.noreply.github.com>

--- tests/integration/test_database_connection.py
+++ tests/integration/test_database_connection.py
@@ -73,7 +73,6 @@ class TestDataBaseConnexion(unittest.TestCase):
 
     @unittest.skip  # Skip because it need the db to be set up
     def test_get_systems_near_location(self):
-
         system = Systems()
         response = system.get_systems_near_location(location=[0.0, 0.0])
         assert isinstance(response, pd.DataFrame)
