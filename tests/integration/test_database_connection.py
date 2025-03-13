8dbe2da7ee643371a3bbae1e8c00817110ad102c
fortinmax <maxime.fortin@rte-france.com>
2025-03-13 14:06:04 +0100

    skip all tests that need a bd
    Signed-off-by: fortinmax <maxime.fortin@rte-france.com>

--- tests/integration/test_database_connection.py
+++ tests/integration/test_database_connection.py
@@ -35,10 +35,12 @@ class TestDataBaseConnexion(unittest.TestCase):
         # Initialize database object
         self.database = DataBase(config)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_sql_db_available(self):
 
         assert self.di.check_sql_available() == True
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_prediction_jobs(self):
 
         pj_retriever = PredictionJobRetriever()
@@ -47,6 +49,7 @@ class TestDataBaseConnexion(unittest.TestCase):
         assert isinstance(response, list)
         assert isinstance(response[0], PredictionJobDataClass)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_pids_for_api_key(self):
 
         pj_retriever = PredictionJobRetriever()
@@ -54,6 +57,7 @@ class TestDataBaseConnexion(unittest.TestCase):
         response = pj_retriever.get_pids_for_api_key("random_api_key")
         assert isinstance(response, list)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_ean_for_pid(self):
 
         pj_retriever = PredictionJobRetriever()
@@ -61,6 +65,7 @@ class TestDataBaseConnexion(unittest.TestCase):
         response = pj_retriever.get_ean_for_pid(1)
         assert isinstance(response, list)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_add_quantiles_to_prediction_jobs(self):
 
         pj_retriever = PredictionJobRetriever()
@@ -70,27 +75,32 @@ class TestDataBaseConnexion(unittest.TestCase):
         assert isinstance(response, PredictionJobDataClass)
         assert hasattr(response, "quantiles")
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_systems_near_location(self):
 
         system = Systems()
         response = system.get_systems_near_location(location=[0.0, 0.0])
         assert isinstance(response, pd.DataFrame)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_systems_by_pid(self):
         system = Systems()
         response = system.get_systems_by_pid(pid=1)
         assert isinstance(response, pd.DataFrame)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_random_pv_systems(self):
         system = Systems()
         response = system.get_random_pv_systems()
         assert isinstance(response, pd.DataFrame)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_api_key_for_system(self):
         system = Systems()
         response = system.get_api_key_for_system(sid="1")
         assert isinstance(response, str)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_power_curve(self):
         modelinput = ModelInput()
         response = modelinput.get_power_curve(turbine_type="Enercon E101")
@@ -108,6 +118,7 @@ class TestDataBaseConnexion(unittest.TestCase):
         ]:
             assert key in response
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_energy_split_coefs(self):
         splitting = Splitting()
         pj_retriever = PredictionJobRetriever()
@@ -116,12 +127,14 @@ class TestDataBaseConnexion(unittest.TestCase):
 
         assert isinstance(response, dict)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_weather_forecast_locations(self):
         weather = Weather()
         response = weather.get_weather_forecast_locations()
 
         assert isinstance(response, list)
 
+    @unittest.skip  # Skip because it need the db to be set up
     def test_get_coordinates_of_location(self):
         weather = Weather()
         response = weather._get_coordinates_of_location(location_name="Leeuwarden")
